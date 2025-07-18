49224eef84a720641bf404ece89e63fea0dae9a0
AuraKai <wehttam1989@gmail.com>
2025-07-18 08:41:01 -0600

    initial project structure: add core modules, models, configuration files, and UI components

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,16 +14,15 @@ dependencyResolutionManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-}
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        mavenLocal()
-        google()
-        mavenCentral()
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/libs.versions.toml"))
+        }
     }
 }
+
+
 
 rootProject.name = "AuraFrameFX"
 include(":app")
