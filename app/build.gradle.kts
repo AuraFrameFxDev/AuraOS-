78574787d05da69945e309285ce4a5e05a4eb36d
AuraKai <wehttam1989@gmail.com>
2025-08-22 11:12:55 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -293,6 +293,8 @@ dependencies {
 
     coreLibraryDesugaring(libs.coreLibraryDesugaring)
 
+implementation "com.google.android.material:material:1.11.0"
+implementation "androidx.compose.material3:material3:1.2.1"
     implementation(platform(libs.firebase.bom))
     implementation(libs.bundles.firebase)
 
